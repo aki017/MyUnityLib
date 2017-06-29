51b30e52016a32e469aa14da658414cc9ce4c602
aki017 <aki@aki017.info>
2017-06-30 06:36:58 +0900

    CaptureUtils

---
+++ CaptureUtils/CaptureUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CaptureUtils
+{
+    public static byte[] TakeScreenShot()
+    {
+        var img = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        img.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        return img.EncodeToPNG();
+    }
+}
